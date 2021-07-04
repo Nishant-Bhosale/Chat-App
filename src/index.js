b36b13a38d5b2e9f6e7f4c1918959d8c32a881d9
Nishant <nishantbhosale244@gmail.com>
2021-07-04 14:38:14 +0530

    Added socket.io library and initialized connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
 const app = express();
+const socketio = require("socket.io");
 const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, "../public");
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+	console.log("New webSocket Connection detected");
+});
+
+server.listen(port, () => {
 	console.log(`port is running on ${port}`);
 });
