5fc86dceb1718ec6810e28c8f2148938371b9945
Nishant <nishantbhosale244@gmail.com>
2021-07-05 12:37:41 +0530

    Learned to share location through geolocation api

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,7 +2,7 @@ const socket = io();
 
 const submitButton = document.querySelector("#submit");
 const inputText = document.querySelector("#client-message");
-
+const sendLocationBtn = document.querySelector("#send-location");
 // socket.on("updatedCount", (count) => {
 // 	console.log("The count is ", count);
 // });
@@ -11,6 +11,19 @@ submitButton.addEventListener("click", () => {
 	// console.log("Clicked");
 
 	socket.emit("sendMessage", inputText.value);
+});
+
+sendLocationBtn.addEventListener("click", () => {
+	if (!navigator.geolocation) {
+		return alert("Geolocation API is not supported.");
+	}
+
+	let locationObj = { latitude: null, longitude: null };
+	navigator.geolocation.getCurrentPosition((location) => {
+		locationObj.latitude = location.coords.latitude;
+		locationObj.longitude = location.coords.longitude;
+		socket.emit("sendLocation", locationObj);
+	});
 });
 
 socket.on("welcomeMessage", (message) => {
--- src/index.js
+++ src/index.js
@@ -26,6 +26,13 @@ io.on("connection", (socket) => {
 		io.emit("message", message);
 	});
 
+	socket.on("sendLocation", (location) => {
+		io.emit(
+			"message",
+			`https://google.com/maps?q=${location.latitude},${location.longitude}`,
+		);
+	});
+
 	socket.on("disconnect", () => {
 		io.emit("message", "A user has left");
 	});
